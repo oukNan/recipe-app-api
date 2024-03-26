783251d1fd48416d89da95644275ddacea6af441
oukNan <oukiepokie@gmail.com>
2024-03-25 21:48:21 -0700

    update

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,43 @@
+
+"""
+Test Django mgmt commands
+"""
+
+
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    def test_wait_for_db_ready(self, patched_check):
+        """
+        queue in wait
+        """
+        patched_check.return_value= True
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(database=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """
+        waiting for data base when Operational Error
+        First 2 times, raise Psycopg2error
+        if next 3 times then raise the OperationalError
+        """
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(database=['default'])
+
+
+
+
