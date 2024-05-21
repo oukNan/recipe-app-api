34d13079305427e1ae2af1e6322025ed94075684
Nan Ouk <38890764+oukNan@users.noreply.github.com>
2024-05-21 14:24:13 -0700

    Update test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
   def test_create_user_with_email_successful(self):
-    email = 'test@example.co
+    email = 'test@example.com'
     password = 'testpass123'
     user = get_user_model().objects.create_user(
       email = email,
